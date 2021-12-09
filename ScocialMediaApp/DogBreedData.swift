53bedbc12009ae3ef8147a61bc82fa000cde57c4
lUrrutiaH <90396675+lUrrutiaH@users.noreply.github.com>
2021-12-09 09:47:51 -0600

    work, please, for the love of god.

---
+++ ScocialMediaApp/DogBreedData.swift
@@ -0,0 +1,13 @@
+//
+//  DogBreedData.swift
+//  ScocialMediaApp
+//
+//  Created by AMStudent on 12/7/21.
+//
+
+import Foundation
+
+struct DogBreedData: Decodable, Identifiable {
+    var id: Int
+    let name: String
+}
---
+++ ScocialMediaApp/FORDOGAPP/ViewController2.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController2.swift
+//  ScocialMediaApp
+//
+//  Created by AMStudent on 12/7/21.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController2: ObservableObject {
+    
+    @Published var DogBreed = [DogBreedData]()
+    let apiURL =  "https://firebasestorage.googleapis.com/v0/b/dogapp-cd081.appspot.com/o/dogapp-cd081-default-rtdb-export.json?alt=media&token=51d201fd-50b0-420b-a4e6-fe040cd9b39f"
+    
+    init() {
+        fetchDogBreedData()
+    }
+    
+  
+    
+    
+    
+  
+    func fetchDogBreedData() {
+        guard let url = URL(string: apiURL) else { return }
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            guard let cleanData = data?.parseData(removeString: "null,") else { return }
+            
+            DispatchQueue.main.async {
+                do{
+                    let Breed = try
+                JSONDecoder().decode([DogBreedData].self, from:
+                        cleanData)
+                    self.DogBreed = Breed
+                } catch {
+            
+                    print("error msg:", error)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    @IBAction func onShareTapped() {
+        let activityController = UIActivityViewController(activityItems: [textField.text!, #imageLiteral(resourceName: "Cosmos07")],
+                                                          applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar! {
+        
+    }
+    
+    
+    
+    
+    
+}
+extension Data {
+    func parseData(removeString string: String) -> Data? {
+        let dataAsString = String(data: self, encoding: .utf8)
+        let parsedDataString = dataAsString?
+            .replacingOccurrences(of: string, with: "")
+        guard let data = parsedDataString?.data(using: .utf8)
+        else { return nil }
+        return data
+        }
+    }
+
